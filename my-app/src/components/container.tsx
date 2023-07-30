22489eaff9e259f8f410ce8238b2b7485feaa16c
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-07-31 01:16:30 +0530

    style add

---
+++ my-app/src/components/container.tsx
@@ -0,0 +1,11 @@
+export const Container = () => {
+
+    return (
+
+        <div style={{ border: '1px solid black', padding: '1rem' }}>
+            
+            Text content goes here 
+
+        </div>
+    )
+}
