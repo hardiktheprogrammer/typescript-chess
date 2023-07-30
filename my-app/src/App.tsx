f1e15ce9f9f0249eccb373093737fa31b0a115d2
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-07-31 01:56:31 +0530

    add  useState hook

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,35 +1,12 @@
-import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/person';
-import { PersonList } from './components/PersonList';
+import './App.css'
+import { Container } from './components/container';
 function App() {
-  const personName = {
-    first: 'Yog',
-    last: 'Akash'
-  };
 
-  const nameList = [
-    {
-    
-    first: 'Brulie',
-    last: 'chose'
-
-
-
-    },
-    {
-      first: 'Choslie',
-      last: 'moslie'
-    }
-  ]
- 
   return (
-    <div className="App">
-      <Greet name='Jiya Band' messageCount={20} isLoggedIn={true} />
-      <Person name= {personName} />
-      <PersonList name= {nameList} />
+  <div  className='App'>
+    <Container styles={{border:'2px solid black', padding:'1rem' , display:'0'} } />
     </div>
-  );
+    
+  )
 }
-
 export default App;
--- my-app/src/components/container.tsx
+++ my-app/src/components/container.tsx
@@ -1,9 +1,12 @@
-export const Container = () => {
+type ContainerProps = {
+    styles:React.CSSProperties
+}
+
+export const Container = (props:ContainerProps) => {
 
     return (
 
-        <div style={{ border: '1px solid black', padding: '1rem' }}>
-            
+        <div style={props.styles}>
             Text content goes here 
 
         </div>
